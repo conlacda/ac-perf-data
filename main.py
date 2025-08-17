f3e8b4632bbc58e7b2bd6367f62ff461390697d1
conlacda <starbrawl72@gmail.com>
2025-08-17 20:08:51 +0900

    Update perfs after contest abc419

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def create_jobs_from_contests_list():
         if not contest.is_rated:
             continue
 
-        schedule.every(2).minutes.until(timedelta(minutes=40)).do(
+        schedule.every(2).minutes.until(timedelta(minutes=60)).do(
             generate_performance_files, contest=contest, commit=False
         )
 
