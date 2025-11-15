0b761d22e7292552e6244ca6b67e1c17c7f72418
phamlong15297 <32757289+phamlong15297@users.noreply.github.com>
2025-11-15 21:29:03 +0900

    Create data/ahc056_contest_type.json

--- contest.py
+++ contest.py
@@ -137,6 +137,17 @@ class Contest:
             if item.get("IsRated"):
                 user = User(item.get("UserScreenName"))
                 user.competition_history(self.type, refresh=True)
+    
+    def sync_competitive_history(self):
+        from user import User
+        res = fetch(RESULT_URL.format(self.short_name), "json")
+        if len(res) == 0:
+            print("Fetch failed")
+            return
+        for item in tqdm(res):
+            if item.get("IsRated"):
+                user = User(item.get("UserScreenName"))
+                user.competition_history(self.type, refresh=True)
 
     # Cập nhật lịch sử thi đấu của toàn bộ người dùng
     def update_competition_history_if_fixed_result_available(self) -> bool:
